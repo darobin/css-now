d45659209c3c3fd73cd9d9787f804d60037d10da
Robin Berjon <robin.berjon@nytimes.com>
2018-09-07 12:16:41 -0400

    will switch

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 
+// -    "cssnano": "^3.10.0",
+// +    "cssnano": "^4.1.0",
+// -    "postcss": "^5.2.15",
+// +    "postcss": "^7.0.2",
+// -    "postcss-cli": "^2.6.0",
+// -    "postcss-cssnext": "^2.9.0",
+// -    "postcss-import": "^9.1.0",
+// -    "postcss-reporter": "^3.0.0",
+// -    "postcss-url": "^5.1.2"
+// +    "postcss-cli": "^6.0.0",
+// +    "postcss-cssnext": "^3.1.0",
+// +    "postcss-import": "^12.0.0",
+// +    "postcss-reporter": "^6.0.0",
+// +    "postcss-url": "^8.0.0"
+
 var fs = require('fs')
   , path = require('path')
-  , npmPath = require('npm-path')
   , join = path.join
-  , spawn = require('child_process').spawn
-  , once = require('once')
-  , devConfig = join(__dirname, 'dev.json')
-  , prodConfig = join(__dirname, 'prod.json')
-  , dev = JSON.parse(fs.readFileSync(devConfig))
-  , prod = JSON.parse(fs.readFileSync(prodConfig))
 ;
 
 module.exports = function (options, cb) {
   var args = [];
   npmPath({ cwd: __dirname }, function (err) {
     if (err) return cb(err);
+    if (options.input) args.push(options.input);
     if (options.watch) args.push('--watch');
     args.push('--config');
     args.push((process.env.NODE_ENV === 'production') ? prodConfig : devConfig);
@@ -22,10 +31,7 @@ module.exports = function (options, cb) {
       args.push('--output');
       args.push(options.output);
     }
-    if (options.input) {
-      args.push('--input');
-      args.push(options.input);
-    }
+    console.log('postcss', args);
     var child = spawn('postcss', args, { stdio:  ['inherit', 'inherit', 'pipe'] })
       , end = once(cb)
       , error = null
